92dd996ff628afa4d5f27677a616b4332b1791df
Mukund Vijayaraghavan <mukund.vijayaraghavan@nyu.edu>
2021-05-09 19:07:05 -0400

    Define create_plan

--- amazon_robot/exercise/multi-robot.py
+++ amazon_robot/exercise/multi-robot.py
@@ -115,8 +115,33 @@ class WarehouseController:
 
     # TODO: Use this function to create your plan
     def create_plan(self):
-        pass
+        poses_robot1 = [get_pose_stamped(**pallets['pallet_1']),
+                        get_pose_stamped(**storage_locations['storage_location_1']),
+                        get_pose_stamped(**free_area['left_end_of_corridor']),
+                        get_pose_stamped(**pallets['pallet_3']),
+                        get_pose_stamped(**storage_locations['storage_location_3'])]
 
+        loads_robot1 = [lift_stages['load'], lift_stages['unload'], lift_stages['unchanged'], lift_stages['load'], lift_stages['unload']]
+        
+        poses_robot2 = [get_pose_stamped(**free_area['right_end_of_corridor']),
+                        get_pose_stamped(**pallets['pallet_2']),
+                        get_pose_stamped(**storage_locations['storage_location_2']),
+                        get_pose_stamped(**free_area['right_end_of_corridor']),
+                        get_pose_stamped(**pallets['pallet_4'])]
+
+        loads_robot2 = [lift_stages['unchanged'], lift_stages['load'], lift_stages['unload'], lift_stages['unchanged'], lift_stages['load']]
+        #
+        # print("Poses List ")
+        # print(poses_robot1)
+        # print("Load List ")
+        # print(loads_robot1)
+        print("Sending target goals to robot1 and robot2")
+
+        self.action_client1.send_targets(poses_robot1, loads_robot1)
+        self.action_client2.send_targets(poses_robot2, loads_robot2)
+
+
+        
     def create_test_plan(self):
         poses_robot1 = [get_pose_stamped(**pallets['pallet_1']),
                         get_pose_stamped(**storage_locations['storage_location_1']),
@@ -128,6 +153,7 @@ class WarehouseController:
         poses_robot2 = [get_pose_stamped(**free_area['right_end_of_corridor']),
                         get_pose_stamped(**pallets['pallet_2']),
                         get_pose_stamped(**storage_locations['storage_location_2']),
+
                         get_pose_stamped(**pallets['pallet_4']),
                         get_pose_stamped(**pallets['pallet_1'])]
 
@@ -181,7 +207,7 @@ class FollowTargetActionClient(Node):
 def main(args=None):
     rclpy.init(args=args)
     warehouse_controller = WarehouseController()
-    warehouse_controller.create_test_plan()
+    warehouse_controller.create_plan()
     warehouse_controller.execute_plan()
 
 
